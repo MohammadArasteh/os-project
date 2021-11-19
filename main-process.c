0ae262180ded34a3f9c0c1ec6998f24c98194f1b
Mew <mohammadkb79@gmail.com>
2021-11-19 15:31:01 +0330

    feat(main-process): create named-pipe between main process and children

--- main-process.c
+++ main-process.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+
+int create_pipe(char *addr);
+
+static char *main_pipe_decoder_address = "./pipes/main_pipe_decoder";
+static char *main_pipe_finder_address = "./pipes/main_pipe_finder";
+static char *main_pipe_placer_address = "./pipes/main_pipe_placer";
+static char *decoder_pipe_finder_address = "./pipes/decoder_pipe_finder";
+static char *finder_pipe_placer_address = "./pipes/finder_pipe_placer";
 
 int main() {
 
@@ -15,5 +26,24 @@ int main() {
     if(decoder_pid) finder_pid = fork();
     if(decoder_pid && finder_pid) placer_pid = fork();
 
+    if(decoder_pid && finder_pid && placer_pid) {
+        if(!create_pipe(main_pipe_decoder_address)) return 1;
+        if(!create_pipe(main_pipe_finder_address)) return 1;
+        if(!create_pipe(main_pipe_placer_address)) return 1;
+    }
+
     return 0;
+}    
+
+struct stat st = {0};
+int create_pipe(char *addr) {
+    if(stat("./pipes", &st) == -1)
+        mkdir("./pipes", 0777);
+    if(mkfifo(addr, 0777) == -1) {
+        if(errno != EEXIST) {
+            printf("creating fifo: unexpected error\n");
+            return 0;
+        }
+    }
+    return 1;
 }
