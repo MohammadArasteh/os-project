c01e9b1752f2f53a767335fa51abadf59f682983
Mew <mohammadkb79@gmail.com>
2021-11-22 01:22:11 +0330

    feat(finder): implement extract_pairs function

--- finder.c
+++ finder.c
@@ -6,6 +6,11 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct pair* extract_pears(char* str);
+struct pair extract_pear(char* str, int *cursor);
+int read_digit(char* str, int* cursor);
+int count_pairs(char* str);
+
 struct pair {
     int start;
     int end;
@@ -31,5 +36,46 @@ int main(int argc, char *argv[]) {
         return 2;
     }
     close(fd);
-    printf("pairs: %s\n", pairs_string);
+    struct pair* pairs = extract_pears(pairs_string);
+}
+
+struct pair* extract_pears(char* str) {
+    int count = count_pairs(str);
+    int cursor = 0;
+    struct pair* pairs = malloc(count * sizeof(struct pair));
+    
+    int index = 0;
+    while(str[cursor] != 0) 
+        pairs[index++] = extract_pear(str, &cursor);
+    
+    return pairs;
+}
+struct pair extract_pear(char* str, int* cursor) {
+    int start = read_digit(str, cursor);
+    int end = read_digit(str, cursor);
+    struct pair p = {start = start, end = end};
+    return p;
+}
+int read_digit(char* str, int* cursor) {
+    int temp = *cursor;
+    char* temp_string = malloc(8);
+    while(str[*cursor] != ' ' && str[*cursor] != '$') {
+        temp_string[*cursor - temp] = str[*cursor];
+        *cursor += 1;
+    }
+    temp_string[*cursor] = 0;
+    *cursor += 1;
+    int digit = atoi(temp_string);
+    free(temp_string);
+    return digit;
+}
+
+int count_pairs(char* str) {
+    int index = 0;
+    int counter = 0;
+    while(str[index] != 0) {
+        if(str[index] == '$') counter++;
+        index++;
+    }
+    return counter;
 }
