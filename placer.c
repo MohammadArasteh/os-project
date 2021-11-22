f2f7c830b89c0b8948bd79a21fc7190c555a4844
Mew <mohammadkb79@gmail.com>
2021-11-23 00:21:54 +0330

    feat(placer): get input data from main process with pipe

--- placer.c
+++ placer.c
@@ -12,4 +12,20 @@ int main(int argc, char *argv[]) {
     int buffer = atoi(argv[1]);
     char *pipe_path = argv[2];
     char *finder_pipe_path = argv[3];
+
+    // opening fifo file (aka named-pipe)
+    int fd = open(pipe_path, O_RDONLY);
+    if(fd == -1) {
+        printf("opening pipe(placer): unexpected error\n");
+        return 2;
+    }
+
+    // getting input from parent process
+    char template[buffer];
+    if((read(fd, &template, buffer + 1)) == -1) {
+        printf("reading pipe(placer): unexpected error\n");
+        return 2;
+    }
+    close(fd);
+
 }
